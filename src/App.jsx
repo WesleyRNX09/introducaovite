d0786fc8947513c529f5ea50f9e028c0737d480d
WesleyRNX09 <wesleyberaldipereira@gmail.com>
2026-02-24 11:18:41 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App (){
+  return(
+    <div>
+      <h1>Aula de React com Vite JS</h1>
+      <h2>Estilizção CSS</h2>
+      <p>Eu não fiz nada</p>
+    </div>
+  );
+}
+
+export default App;
