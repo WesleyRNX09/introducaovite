670b127040436e8b12063a705da905635c16a7d6
WesleyRNX09 <wesleyberaldipereira@gmail.com>
2026-03-03 11:45:10 -0300

    aula 03-03

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,11 @@
+
+function Sobre () {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <p>Texto do meu sobre Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, fugit. Voluptas, enim vero! Commodi, delectus reiciendis laborum illum distinctio numquam culpa quas repellendus excepturi sint earum possimus incidunt architecto est.</p>
+        </div>
+    )
+}
+
+export default Sobre;
