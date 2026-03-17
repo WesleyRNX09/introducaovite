33da53832f69297ca1c34c6474514d3f05aae24a
WesleyRNX09 <wesleyberaldipereira@gmail.com>
2026-03-17 11:39:06 -0300

    aula 17-03

--- src/pages/exemplos/ex-01/index.jsx
+++ src/pages/exemplos/ex-01/index.jsx
@@ -1,4 +1,4 @@
-function Exemplo01 () {
+function Exemplo01() {
     return (
         <>
             <h1>Exemplo 1</h1>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,24 +1,23 @@
-
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router";
 
-function Home () {
+import styles from './index.module.css';
 
-    const navigate = useNavigate();
+function Home() {
 
-    return (
-        <div>
-            <h1>Aula de React com Vite JS</h1>
-
-            <button
-            onClick={() => navigate('/exemplo/1')}
-            >
-                exemplo 1
-            </button>
-
-            <Link to="/exemplo/2">Exemplo 2</Link>
+  return (
+    <div className={styles.container}>
+        <h1>Aula de React com Vite JS</h1>
+        <div className={styles.containerCards}>   
+          <h2>Exemplos</h2>       
+          <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
+          <Link to="/exemplo/2">Exemplo 2 - Uso de module CSS</Link>    
         </div>
-    )
+        
+        <div className={styles.containerCards}>    
+          <h2>Atividades</h2>      
+        </div>
+    </div>
+  )
 }
 
 export default Home;
