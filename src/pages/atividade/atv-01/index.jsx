c4fcd2837f9a0ab1ce2828716ae99c44a9ac2761
WesleyRNX09 <wesleyberaldipereira@gmail.com>
2026-03-24 11:42:37 -0300

    aula 24-03

--- src/pages/atividade/atv-01/index.jsx
+++ src/pages/atividade/atv-01/index.jsx
@@ -3,10 +3,10 @@ import styles from './index.module.css';
 function Atividade01() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.text}>Exemplo 1</h1>
+            <h1 className={styles.text}>Atividade 1</h1>
             <p className={styles.text}>Conteúdo mínimo para componente React</p>
            
-            <div>
+            <div className={styles.cores}>
                 <div className={styles.vermelho}></div>
                 <div className={styles.verde}></div>
                 <div className={styles.branco}></div>
